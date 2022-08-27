3562f092dd1edf8933d6c4751c2bb7872fb75f45
Muhammad Ragil <muhammad.ramadhan@btpn.com>
2022-08-27 22:33:30 +0700

    add filter breed functionality

--- src/Pages/HomePage/SearchBar.jsx
+++ src/Pages/HomePage/SearchBar.jsx
@@ -1,8 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import { useState } from 'react';
 import SuggestionBox from './SuggestionBox';
 
 function SearchBar({ placeholderText }) {
   const [isBoxOpen, setIsBoxOpen] = useState(false);
+  const [search, setSearch] = useState('');
+
+  const {
+    isLoading, error, data: suggestions,
+  } = useQuery(['allBreeds'], () => axios
+    .get('http://localhost:3001/cats/breeds/all')
+    .then((res) => res.data), {
+    refetchOnWindowFocus: false,
+  });
 
   const openSuggestion = () => {
     setIsBoxOpen(true);
@@ -12,13 +23,30 @@ function SearchBar({ placeholderText }) {
     setIsBoxOpen(false);
   };
 
+  const handleSearch = (text) => {
+    setSearch(text);
+  };
+
+  if (isLoading) return <div>Loading sayang...</div>;
+
+  if (error) return <div>Error sayang...</div>;
+
+  let filteredSuggestion = suggestions;
+  if (search !== '') {
+    filteredSuggestion = suggestions.filter(
+      (breed) => breed.name
+        .toLowerCase()
+        .includes(search.toLowerCase()),
+    );
+  }
+
   return (
     <div>
       <div className="flex items-center">
-        <input type="text" className="rounded-full p-4 md:w-64" placeholder={placeholderText} onFocus={openSuggestion} onBlur={hideSuggestion} />
+        <input type="text" className="rounded-full p-4 md:w-64 text-black" placeholder={placeholderText} onFocus={openSuggestion} onBlur={hideSuggestion} onChange={(e) => handleSearch(e.target.value)} value={search} />
         <svg fill="none" stroke="black" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" className="w-5 h-5 absolute left-56 md:left-94"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
       </div>
-      {isBoxOpen && <SuggestionBox /> }
+      {isBoxOpen && <SuggestionBox keyword={search} suggestions={filteredSuggestion} /> }
     </div>
   );
 }
--- src/Pages/HomePage/SuggestionBox.jsx
+++ src/Pages/HomePage/SuggestionBox.jsx
@@ -1,30 +1,19 @@
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-function SuggestionBox() {
+function SuggestionBox({ suggestions }) {
   const navigate = useNavigate();
-  const {
-    isLoading, error, data: suggestions,
-  } = useQuery(['allBreeds'], () => axios
-    .get('http://localhost:3001/cats/breeds/all')
-    .then((res) => res.data), {
-    refetchOnWindowFocus: false,
-  });
 
   const navigateToDetails = (breedId) => {
     navigate(`/cat/details/${breedId}`);
   };
 
-  if (isLoading) return <div>Loading sayang...</div>;
-
-  if (error) return <div>Error sayang...</div>;
-
   return (
-    <div className="">
+    <div>
       <div className="flex items-start justify-start mt-2">
-        <div className="p-4 bg-white rounded-3xl text-black h-60 overflow-scroll">
-          <div>{suggestions.map((breed) => <option key={breed.id} className="mb-5 cursor-pointer hover:bg-gray-100" onMouseDown={() => navigateToDetails(breed.id)}>{breed.name}</option>)}</div>
+        <div className={`p-4 bg-white rounded-3xl text-black ${suggestions.length > 1 ? 'h-60' : 'h-14'} w-60 overflow-scroll`}>
+          {suggestions.length > 0
+            ? <div>{suggestions.map((breed) => <option key={breed.id} className="mb-5 cursor-pointer hover:bg-gray-100" onMouseDown={() => navigateToDetails(breed.id)}>{breed.name}</option>)}</div>
+            : <h1>Cat breed not found!</h1> }
         </div>
       </div>
     </div>
